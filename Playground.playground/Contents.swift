4c7cb7a11e18ef1ed8be7fba8d4c92047c0dfe64
User <user@Administrators-Mac-mini.local>
2019-01-30 13:10:11 +0300

    Refactoring detail view

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -29,50 +29,91 @@
     }
  }
  
+ class AnimationCoordinator: NSObject {
+ }
+ 
  class DetailViewController: UIViewController {
     lazy var dataSource = {
        return SampleTableDataSource()
     }()
+    lazy var topView: UIView = {
+        let containerView = UIView(frame: CGRect.zero)
+        containerView.backgroundColor = UIColor.red
+        
+        let label = UILabel(frame: CGRect.zero)
+        label.text = "Detail view controller"
+        containerView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        
+        //adding two gesture recognizers
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        containerView.addGestureRecognizer(tapGestureRecognizer)
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        containerView.addGestureRecognizer(panGestureRecognizer)
+        return containerView
+    }()
+    lazy var tableView: UITableView = {
+        let table = UITableView(frame: CGRect.zero, style: .plain)
+        table.dataSource = dataSource
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "PlainCell")
+        return table
+    }()
+    //gesture handlers
+    var handleTapFunc: (UITapGestureRecognizer) -> Void = {
+        recognizer in
+    }
+    var handlePanFunc: (UIPanGestureRecognizer) -> Void = {
+        recognizer in
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let label = UILabel(frame: CGRect.zero)
-        label.text = "Sam's photo"
-        let tableView = UITableView(frame: CGRect.zero, style: .plain)
-        tableView.dataSource = dataSource
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PlainCell")
-        
         view.backgroundColor = UIColor.white
         
-        view.addSubview(label)
+        view.addSubview(topView)
         view.addSubview(tableView)
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
+    
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        topView.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
-        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        topView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        topView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
         
-        tableView.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
         tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-        tableView.reloadData()
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        handleTapFunc(recognizer)
+    }
+    
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+        handlePanFunc(recognizer)
     }
  }
  
  class MasterViewController: UIViewController {
     lazy var detailViewController: DetailViewController = {
         let vc = DetailViewController(nibName: nil, bundle: nil)
-        vc.view.frame = view.frame.offsetBy(dx: 0.0, dy: view.frame.height - 50.0)
+        vc.view.frame = view.frame.offsetBy(dx: 0.0, dy: view.frame.height/2)
         return vc
     }()
     
     lazy var label: UILabel = {
         let label = UILabel(frame: CGRect.zero)
-        label.text = "Sam's photo"
+        label.text = "Master view controller"
         return label
     }()
     
@@ -103,15 +144,6 @@
         imageView.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
         imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    }
-    
-    @objc func buttonTapped() {
-        UIView.animate(withDuration: 0.5) {
-            [weak detailView = self.detailViewController.view, weak view = self.view] in
-            if let frame = view?.frame {
-                detailView?.frame = frame
-            }
-        }
     }
  }
  
