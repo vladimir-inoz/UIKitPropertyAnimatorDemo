a7b4bbddf37ef3c4f37b308a538b28d7b64458f2
User <user@Administrators-Mac-mini.local>
2019-01-30 12:39:44 +0300

    Completed view controllers

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -25,128 +25,97 @@
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PlainCell", for: indexPath)
         cell.textLabel?.text = stringData[indexPath.row]
-        cell.detailTextLabel?.text = "Testtest"
         return cell
     }
  }
  
  class DetailViewController: UIViewController {
+    lazy var dataSource = {
+       return SampleTableDataSource()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let label = UILabel(frame: CGRect.zero)
         label.text = "Sam's photo"
         let tableView = UITableView(frame: CGRect.zero, style: .plain)
-        let dataSource = SampleTableDataSource()
         tableView.dataSource = dataSource
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "PlainCell")
-        let button = UIButton(type: .system)
-        button.setTitle("Expose", for: .normal)
         
         view.backgroundColor = UIColor.white
         
         view.addSubview(label)
         view.addSubview(tableView)
-        view.addSubview(button)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        button.translatesAutoresizingMaskIntoConstraints = false
         
-        label.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
         tableView.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
-        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: button.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
         tableView.reloadData()
     }
  }
  
  class MasterViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    lazy var detailViewController: DetailViewController = {
+        let vc = DetailViewController(nibName: nil, bundle: nil)
+        vc.view.frame = view.frame.offsetBy(dx: 0.0, dy: view.frame.height - 50.0)
+        return vc
+    }()
+    
+    lazy var label: UILabel = {
         let label = UILabel(frame: CGRect.zero)
         label.text = "Sam's photo"
-        let imageView = UIImageView(image: nil)
-        let button = UIButton(type: .system)
-        button.setTitle("Expose", for: .normal)
+        return label
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let view = UIImageView(image: nil)
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         view.backgroundColor = UIColor.white
         
         view.addSubview(label)
         view.addSubview(imageView)
-        view.addSubview(button)
+        view.addSubview(detailViewController.view)
         
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
         label.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        button.translatesAutoresizingMaskIntoConstraints = false
         
         label.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
         imageView.topAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
         imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: button.topAnchor).isActive = true
     }
     
     @objc func buttonTapped() {
-        
+        UIView.animate(withDuration: 0.5) {
+            [weak detailView = self.detailViewController.view, weak view = self.view] in
+            if let frame = view?.frame {
+                detailView?.frame = frame
+            }
+        }
     }
-    /*var animators: UIViewPropertyAnimator!
-     var circle: UIView!
-     var progressWhenInterrupted: CGFloat = 0
-     
-     func animateTransitionIfNeeded(duration: TimeInterval) {
-     if animator == nil {
-     animator = UIViewPropertyAnimator(duration: duration, curve: .easeOut) {
-     self.circle.frame = self.circle.frame.offsetBy(dx: self.view.frame.width, dy: 0)
-     }
-     animator.pausesOnCompletion = true
-     }
-     }
-     
-     override func viewDidLoad() {
-     circle = UIView(frame: CGRect(x: 20.0, y: 20.0, width: 40.0, height: 40.0))
-     circle.backgroundColor = UIColor.red
-     view.addSubview(circle)
-     
-     let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-     circle.addGestureRecognizer(panRecognizer)
-     }
-     
-     @objc func handlePan(recognizer: UIPanGestureRecognizer) {
-     switch recognizer.state {
-     case .began:
-     print("began")
-     animateTransitionIfNeeded(duration: 15.0)
-     animator.pauseAnimation()
-     progressWhenInterrupted = animator.fractionComplete
-     case .changed:
-     print("changed")
-     let translation = recognizer.translation(in: circle)
-     animator.fractionComplete = (translation.x / view.frame.width) + progressWhenInterrupted
-     case .ended:
-     print("ended")
-     let timing = UICubicTimingParameters(animationCurve: .easeOut)
-     animator.continueAnimation(withTimingParameters: timing, durationFactor: 0)
-     default:
-     break
-     }
-     }*/
  }
  
- let vc = DetailViewController()
+ let vc = MasterViewController()
  
  PlaygroundPage.current.liveView = vc
  PlaygroundPage.current.needsIndefiniteExecution = true
